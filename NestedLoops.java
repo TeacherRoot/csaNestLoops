f67fba5bb265b9cd591db8034446ff41030d4d5c
Ann Root <ann.root@bvsd.org>
2024-10-18 14:52:51 -0600

    Add files via upload

---
+++ NestedLoops.java
@@ -0,0 +1,121 @@
+public class NestedLoops {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Assignment 1: Triangle Letter Pattern: ");
+        triangleLetter(3, 'A');
+        triangleLetter(7, 'x');
+        triangleLetter(1, 'R');
+        triangleLetter(5, 'T');
+        triangleLetter(4, 'W');
+        System.out.println("\n\n");
+        System.out.println("Assignment 2: Flip Triangle Letter Pattern: ");
+        flipTriangleLetter(3, 'A');
+        flipTriangleLetter(7, 'x');
+        flipTriangleLetter(1, 'R');
+        flipTriangleLetter(5, 'T');
+        flipTriangleLetter(4, 'W');
+        System.out.println("\n\n");
+        System.out.println("Assignment 3: Vertical MirrorTriangle Letter Pattern: ");
+        verticalMirrorTriangleLetter(3, 'A');
+        verticalMirrorTriangleLetter(7, 'x');
+        verticalMirrorTriangleLetter(1, 'R');
+        verticalMirrorTriangleLetter(5, 'T');
+        verticalMirrorTriangleLetter(4, 'W');
+        System.out.println("\n\n");
+        System.out.println("Assignment 4: Horizontal Flip Triangle Letter Pattern: ");
+        horizontalTriangleFlip(3, 'A');
+        horizontalTriangleFlip(7, 'x');
+        horizontalTriangleFlip(1, 'R');
+        horizontalTriangleFlip(5, 'T');
+        horizontalTriangleFlip(4, 'W');
+        System.out.println("\n\n");
+        System.out.println("Assignment 5: Box Pattern");
+        boxPattern(3);
+        boxPattern(4);
+        boxPattern(5);
+        boxPattern(2);
+        boxPattern(1);
+        System.out.println("\n\n");
+        System.out.println("Assignment 6: Square Word");
+        squareWord("SQUARE");
+        squareWord("BOX");
+        squareWord("A");
+        squareWord("IT");
+        squareWord("TOAD");
+        System.out.println("\n\n");
+        System.out.println("Assignment 7: Letter Triangle");
+        letterTriangle('C', 4);
+        letterTriangle('A', 5);
+        letterTriangle('B', 7);
+        letterTriangle('X', 6);
+        letterTriangle('Z', 8);
+
+
+
+
+    }
+
+    /* Assignment 1:
+     * Use nested loops to print a triangle with row rows using the letter "letter" 
+     * For example:  triangleLetter(3, 'A') will print 
+     * A
+     * AA
+     * AAA
+    */
+    public static void triangleLetter(int rows, char letter) {
+        
+
+    }
+
+     /* Assignment 2:
+     * Use nested loops to print a triangle with row rows using the letter "letter" 
+     * For example:  flipTriangleLetter(3, 'A') will print 
+     * AAA  
+     * AA
+     * A
+    */
+    public static void flipTriangleLetter(int rows, char letter) {
+
+
+    }
+
+     /* Assignment 3:
+     * Use nested loops to print a triangle with row rows using the letter "letter" 
+     * For example:  verticalMirrorTriangleLetter(3, 'A') will print 
+     *     A
+     *    AA
+     *   AAA
+    */
+    public static void verticalMirrorTriangleLetter(int rows, char letter) {
+
+
+    }
+
+    /* Assignment 4:
+     * Use nested loops to print a triangle with row rows using the letter "letter" 
+     * For example:  horizontalTriangleFlip(3, 'A') will print 
+     * AAA
+     *  AA
+     *   A
+    */
+
+    public static void horizontalTriangleFlip(int rows, char letter) {
+
+    }
+
+    /*  Print out the box pattern with rows rows */
+    public static void boxPattern(int rows) {
+
+    }
+
+    public static void squareWord(String word) {
+
+
+    }
+
+    public static void letterTriangle(char c, int rows) {
+        
+    }
+
+}
